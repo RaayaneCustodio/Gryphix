25681f8c0009d20357b417da8f41923c551c3bb1
RaayaneCustodio <108167166+RaayaneCustodio@users.noreply.github.com>
2025-10-05 19:19:43 -0300

    Criacao do arquivo na database, tipo de dado

--- backend/database/migrations/2025_10_05_215511_create_games_table.php
+++ backend/database/migrations/2025_10_05_215511_create_games_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
-            $table->tipoDeDado('nome_da_coluna');
+            $table->string('title');
             $table->timestamps();
         });
     }
